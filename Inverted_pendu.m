e5cc803d024703ace358e4f883aea0824fdb5d1b
Srinidhi7 <srinidhi.s.bharadwaj007@gmail.com>
2015-08-23 17:51:31 +0530

    Added circle for pendulum bob

--- Inverted_pendu.m
+++ Inverted_pendu.m
@@ -2,7 +2,7 @@
 clear
 clf
 
-Length = 1;
+Length = 10;
 Mass = 15;
 g=9.81;
 
@@ -10,20 +10,22 @@ k=g/Length;
 theta_init=pi+0.01;
 
 t=0;
+
 tic;
 
-    while t<=10
+    while t<=5
         t=toc/10;
-        theta = (pi/18)*cos(time*sqrt(k));       
+      
         theta=(theta_init-(k*pi*t*t))/(1-k*t*t);   
         
         lx=Length*cos(theta-(pi/2));
         ly=Length*sin(theta-(pi/2));
 
-        plot([0 lx],[0 ly],'b','Linewidth',3);   
-        rectangle('Curvature',[1,1],'Position',[lx-0.05,ly-0.05,0.1,0.1],'FaceColor','b');
-
-        axis([-2 2 -2 2])
+        plot([0 lx],[0 ly],'b','Linewidth',1);   
+        %rectangle('Curvature',[1,1],'Position',[lx-0.05,ly-0.05,0.1,0.1],'FaceColor','b');
+        viscircles([lx,ly],Length/20,'EdgeColor','b','LineWidth',1);
+        
+        axis([-Length-1 Length+1 -Length-1 Length+1])
 
         pause(0.01);
         
--- Simple_Pendulum.m
+++ Simple_Pendulum.m
@@ -12,17 +12,17 @@ theta_init=pi/18;
 t=0;
 tic;
 
-    while t<=10
+    while t<=5
         t=toc;
         theta = (theta_init)*cos(t*sqrt(k));         
         
         lx=Length*cos(theta-(pi/2));
         ly=Length*sin(theta-(pi/2));
 
-        plot([0 lx],[0 ly],'b','Linewidth',3);   
-        rectangle('Curvature',[1,1],'Position',[lx-0.05,ly-0.05,0.1,0.1],'FaceColor','b');
-
-        axis([-2 2 -2 2])
+        plot([0 lx],[0 ly],'b','Linewidth',1);   
+        %rectangle('Curvature',[1,1],'Position',[lx-0.05,ly-0.05,0.1,0.1],'FaceColor','b');
+        viscircles([lx,ly],Length/12,'EdgeColor','b','LineWidth',1);
+        axis([-Length-1 Length+1 -Length-1 Length+1])
 
         pause(0.01);
     end
