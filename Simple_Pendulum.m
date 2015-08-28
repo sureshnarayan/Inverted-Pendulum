3cb4d74db973a930d0f10c1807c86cff2a894d13
sureshnarayan <sureshbutterfly@gmail.com>
2015-08-28 17:32:08 +0530

    Damped system solutions incorporated
    Both IP and SP are based on damped eq
    The platform is still stationary

--- Simple_Pendulum.m
+++ Simple_Pendulum.m
@@ -5,24 +5,35 @@ clf
 Length = 1;
 Mass = 15;
 g=9.81;
-c=0.00001;
+c=1;
 
+%specific solution constants
 k=g/Length;
 theta_init=pi/18;
+
+%specific solution parameters 
+p=[1 c k];
+r=roots(p);
+A=[r(1) r(2);1 1];
+B=[0;theta_init];
+C=linsolve(A,B);
+
 
 t=0;
 tic;
 
-    while t<=5
+    while t<=10
         t=toc;
-        %theta = (theta_init*(exp(-c*t)*k/c+1/t))/(1/t+(k/c));         
-        theta = (theta_init)*cos(t*sqrt(k));   
         
+        theta_i = C(1)*exp(r(1)*t)+C(2)*exp(r(2)*t);  %for damped systems        
+        %theta = (theta_init)*cos(t*sqrt(k));  %for undamped system  
+        theta= real(theta_i);
+        
+        %plotting 
         lx=Length*cos(theta-(pi/2));
         ly=Length*sin(theta-(pi/2));
 
-        plot([0 lx],[0 ly],'b','Linewidth',1);   
-        %rectangle('Curvature',[1,1],'Position',[lx-0.05,ly-0.05,0.1,0.1],'FaceColor','b');
+        plot([0 lx],[0 ly],'b','Linewidth',1);  
         viscircles([lx,ly],Length/12,'EdgeColor','b','LineWidth',1);
         axis([-Length-1 Length+1 -Length-1 Length+1])
 
